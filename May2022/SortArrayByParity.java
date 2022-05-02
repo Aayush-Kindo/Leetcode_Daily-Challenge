e8fe44f3e067cb16826c036ce22180c74e2d2e00
Aayush Kindo <aayush.kindo@gmail.com>
2022-05-02 11:58:16 +0530

    Create SortArrayByParity.java

---
+++ May2022/SortArrayByParity.java
@@ -0,0 +1,21 @@
+public int[] sortArrayByParity(int[] nums) {
+        int i=0;
+        int j=0;
+        
+        while(i<nums.length){
+           if(nums[i]%2==0) {
+               swap(nums,i,j);
+               i++;
+               j++;
+           }else{
+               i++;
+           }
+        }
+        return nums;
+    }
+    
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
